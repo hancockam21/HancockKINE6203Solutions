730c3cd656a3f851130e6d1fc07ea7dc3bfe2e7c
hancockam21 <hancockam21@students.ecu.edu>
2023-09-11 13:49:56 -0400

    Tic Tac Toe

--- TicTacToe.m
+++ TicTacToe.m
@@ -1,13 +1,19 @@
 % Tic Tac Toe Game
 % Amber Hancock 
 
-% Detailed Description 
-% What should a user do 
+% This code is intended to produce a Tic-Tac-Toe game that is able to be
+% played by the user through their input and has a computer generated
+% opponent. 
+
+% The user will be prompted various times to input a response such as yes I
+% would like to play or where would the user like to place a move. Once the
+% user hits return after inputting their answer, the computer will also
+% generate a move. 
 
 % Tested using Matlab R2023a
 % Tested on Macbook Air 
 
-% Last updated 09/07/2023
+% Last updated 09/11/2023
 
 % Welcome message 
 
@@ -19,43 +25,63 @@ if (userInput == "Y")
     disp('playing game')
 
     gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "H" "I"];
+    % gameboard is shown to user
     disp(gameboard)
 
-    userMove = input("Enter the letter of the square you'd like to place your first move; ", "s")
-    % put if user inputs ____, change that square to X. and so on. 
 
-        if(userInput == "A")
+    % Player's first move. 
+    userMoveOne = input("Enter the letter of the square you'd like to place your first move. ", "s")
+    % put if user inputs ____, change that square to X. and so on. 
+    
+        if(userMoveOne == "A")
            
            gameboard = ["X" "B" "C"; "D" "E" "F"; "G" "H" "I"];
            disp(gameboard)
 
-        elseif(userInput == "B")
+        elseif(userMoveOne == "B")
             gameboard = ["A" "X" "C"; "D" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-        elseif(userInput == "C")
+        elseif(userMoveOne == "C")
             gameboard = ["A" "B" "X"; "D" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-        elseif(userInput == "D")
+        elseif(userMoveOne == "D")
             gameboard = ["A" "B" "C"; "X" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-        elseif(userInput == "E")
+        elseif(userMoveOne == "E")
             gameboard = ["A" "B" "C"; "D" "X" "F" ; "G" "H" "I"]
             disp(gameboard)
-        else if(userInput == "F")
+        elseif(userMoveOne == "F")
             gameboard = ["A" "B" "C"; "D" "E" "X" ; "G" "H" "I"]
-        else if(userInput == "G")
+        elseif(userMoveOne == "G")
             gameboard = ["A" "B" "C"; "D" "E" "F" ; "X" "H" "I"]
             disp(gameboard)
-        else if(userInput == "H")
+        elseif(userMoveOne == "H")
             gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "X" "I"]
             disp(gameboard)
-        elseif(userInput == "I")
+        elseif(userMoveOne == "I")
             gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "H" "X"]
-        else 
-            disp('invalid user input')
+        else
+            disp("invalid user input")
+        end
+    % generate a value of "O" in one of the matrix entries that is not 
+    % already equal to X (any matrix location that is still a letter)
+
+        % computerMoveOne = rand(gameboard) ????
+
+    % for when a matrix has a value not equal to X, computer needs to
+    % randomly select a element to change to O. 
+
+   
+    
+    
+% After 5 moves (minimum number of moves possible), you'll need to code for
+% if there is 3 X or O in a row. (diagonal or across or down) 
+% if there is 3 X in a row, disp 'you won!'. If there are 3 O in a row,
+% disp 'game over' 
+    
+
+else 
+    disp("quitting game")
+    % quit game 
         end
         
-else 
-    disp('quitting game')
-    % quit game 
-end 
