28877ef9b95f5f29909f76035266f65c8daba796
hancockam21 <hancockam21@students.ecu.edu>
2023-09-15 19:14:19 -0400

    tic tac toe

--- TicTacToe.m
+++ TicTacToe.m
@@ -6,7 +6,7 @@
 % opponent. 
 
 % The user will be prompted various times to input a response such as yes I
-% would like to play or where the user would like to place a move. Once the
+% would like to play or where would the user like to place a move. Once the
 % user hits return after inputting their answer, the computer will also
 % generate a move. 
 
@@ -18,189 +18,55 @@
 % Welcome message 
 
 disp("Welcome to Tic Tac Toe. Play well and good luck!")
-% user is then asked if they would like to play. 
-userInput = input("Do you want to play? Y/N: (Enter a Y or a N and press return) ", "s");
 
-    if (userInput == "Y")
+userInput = input("Do you want to play? Y/N: (Enter a Y or a N and press return) ", "s")
+if (userInput == "Y")
+
     % gameboard is shown to user
-    gameboard = ["A" "B" "C"; "D" "E" "F"; "G" "H" "I"];
     disp(gameboard)
 
- avail = ["A" "B" "C" "D" "E" "F" "G" "H" "I"];
 
     % Player's first move. 
-    userMoveOne = input("Enter the letter of the square you'd like to place your first move. ", "s");
-    % if user inputs ____, change that square to X. and so on. 
+    userMoveOne = input("Enter the letter of the square you'd like to place your first move. ", "s")
+    % put if user inputs ____, change that square to X. and so on. 
     
-  avail = ["A" "B" "C"; "D" "E" "F"; "G" "H" "I"];  
-    % this is indicating the available inputs the user can choose from to
-    % place their first move.
-
         if(userMoveOne == "A")
-           posUserMoveOne = [1,1]; % setting location of that move to a position 
-                               % in the matrix 
+           
            gameboard = ["X" "B" "C"; "D" "E" "F"; "G" "H" "I"];
            disp(gameboard)
-           unavail = "A" ;
 
         elseif(userMoveOne == "B")
-            posUserMoveOne = [1,2];
-            gameboard = ["A" "X" "C"; "D" "E" "F" ; "G" "H" "I"];
+            gameboard = ["A" "X" "C"; "D" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-            unavail = "B" ;
-
         elseif(userMoveOne == "C")
-             posUserMoveOne = [1,3];
-            gameboard = ["A" "B" "X"; "D" "E" "F" ; "G" "H" "I"];
+            gameboard = ["A" "B" "X"; "D" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-            unavail = "C" ;
-
         elseif(userMoveOne == "D")
-             posUserMoveOne = [2,1];
-            gameboard = ["A" "B" "C"; "X" "E" "F" ; "G" "H" "I"];
+            gameboard = ["A" "B" "C"; "X" "E" "F" ; "G" "H" "I"]
             disp(gameboard)
-            unavail = "D" ;
-
         elseif(userMoveOne == "E")
-            posUserMoveOne = [2,2];
-            gameboard = ["A" "B" "C"; "D" "X" "F" ; "G" "H" "I"];
+            gameboard = ["A" "B" "C"; "D" "X" "F" ; "G" "H" "I"]
             disp(gameboard)
-            unavail = "E" ;
-
         elseif(userMoveOne == "F")
-            posUserMoveOne = [2,3];
-            gameboard = ["A" "B" "C"; "D" "E" "X" ; "G" "H" "I"];
-            disp(gameboard)
-            unavail = "F" ;
-
+            gameboard = ["A" "B" "C"; "D" "E" "X" ; "G" "H" "I"]
         elseif(userMoveOne == "G")
-            posUserMoveOne = [3,1];
-            gameboard = ["A" "B" "C"; "D" "E" "F" ; "X" "H" "I"];
+            gameboard = ["A" "B" "C"; "D" "E" "F" ; "X" "H" "I"]
             disp(gameboard)
-            unavail = "G" ;
-
         elseif(userMoveOne == "H")
-             posUserMoveOne = [3,2];
-            gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "X" "I"];
+            gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "X" "I"]
             disp(gameboard)
-            unavail = "H" ;
-
         elseif(userMoveOne == "I")
-             posUserMoveOne = [3,3];
-            gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "H" "X"];
-            disp(gameboard)
-            unavail = "I" ;
-
+            gameboard = ["A" "B" "C"; "D" "E" "F" ; "G" "H" "X"]
         else
             disp("invalid user input")
         end
+    % generate a value of "O" in one of the matrix entries that is not 
+    % already equal to X (any matrix location that is still a letter)
 
-avail = setdiff(avail, unavail); % only allow spaces that weren't used in 
-                                 % move one to be available. 
+        % computerMoveOne = rand(gameboard) ????
 
-compMoveOne = randi(length(avail),1);
-
-    if avail(1,compMoveOne) == "A"
-        gameboard(1,1) = "O";
-        unavail = "A";
-        disp(gameboard)
-    
-    elseif avail(1, compMoveOne) == "B" 
-        gameboard(1,2) = "O";
-        unavail = "B";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "C" 
-        gameboard(1,3) = "O";
-        unavail = "C";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "D"
-        gameboard(2, 1) = "O";
-        unavail = "D";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "E"
-        gameboard(2,2) = "O";
-        unavail = "E";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "F"
-        gameboard(2,3) = "O";
-        unavail = "F";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "G"
-        gameboard(3,1) = "O" ;
-        unavail = "G" ;
-        disp(gameboard) 
-
-    elseif avail(1, compMoveOne) == "H"
-        gameboard(3,2) = "O" ;
-        unavail = "H";
-        disp(gameboard)
-
-    elseif avail(1, compMoveOne) == "I"
-        gameboard(3,3) = "O";
-        unavail = "I";
-        disp(gameboard)
-    end 
-
-avail = setdiff(avail, unavail);  % updating available moves
-userMoveTwo = input("Where would you like to place your second move?" "s")
-    
-    if userMoveTwo = "A"
-        gameboard(1,1) = "X" ;
-        unavail = "A" ; 
-        disp(gameboard)
-
-    elseif userMoveTwo = "B" 
-        gameboard(1,2) = "X" ; 
-        unavail = "B" ;
-        disp(gameboard) 
-
-    elseif userMoveTwo = "C" 
-        gameboard(1,3) = "X" ;
-        unavail = "C" ;
-        disp(gameboard)
-        
-    elseif userMoveTwo = "D" 
-        gameboard(2,1) = "X" ;
-        unavail = "D" ; 
-        disp(gameboard)
-        
-    elseif userMoveTwo = "E"
-        gameboard(2,2) = "X" ;
-        unavail = "E" ;
-        disp(gameboard)
-
-    elseif userMoveTwo = "F" 
-        gameboard(2,3) = "X" ; 
-        unavail = "F" ;
-        disp(gameboard) 
-
-    elseif userMoveTwo = "G" 
-        gameboard(3,1) = "X" ;
-        unavail = "G" ;
-        disp(gameboard)
-
-    elseif userMoveTwo = "H" 
-        gameboard(3,2) = "X" ;
-        unavail = "H" ;
-        disp(gameboard)
-        
-    elseif userMoveTwo = "I"
-        gameboard(3,3) = "X" ;
-        unavail = "I" ;
-        disp(gameboard)
-    end 
-
-
-
-    
-
-       
-
+    % for when a matrix has a value not equal to X, computer needs to
+    % randomly select a element to change to O. 
 
    
     
@@ -211,9 +77,8 @@ userMoveTwo = input("Where would you like to place your second move?" "s")
 % disp 'game over' 
     
 
-
 else 
     disp("quitting game")
     % quit game 
-    end
+        end
         
