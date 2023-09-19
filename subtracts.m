2e24ff289fedc2b0468f2c8de347dbb08378cb30
hancockam21 <hancockam21@students.ecu.edu>
2023-09-19 12:16:24 -0400

    Tic Tac Toe

---
+++ subtracts.m
@@ -0,0 +1,9 @@
+function subtracts(inputArg1,inputArg2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+if (inputArg1 > inputArg2)
+    inputArg1 - inputArg2
+else 
+    inputArg2 - inputArg1
+end
+end
