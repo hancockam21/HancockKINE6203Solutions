ddb63632550e277ad5b41ada48fc313c1f3e066b
hancockam21 <hancockam21@students.ecu.edu>
2023-09-26 12:14:16 -0400

    updated

---
+++ money.m
@@ -0,0 +1,5 @@
+function [totalCoins] = money(quarters, dimes, nickels, pennies)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+totalCoins = (0.25*quarters) + (0.10*dimes) + (0.05*nickels) + (0.01*pennies) ;
+end
