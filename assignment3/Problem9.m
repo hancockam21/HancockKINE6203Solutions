33c2c082dc4434510789d1e746ceab01d6a9d3e0
hancockam21 <hancockam21@students.ecu.edu>
2023-10-04 20:17:39 -0400

    Updated

---
+++ assignment3/Problem9.m
@@ -0,0 +1,8 @@
+function [lengthOfString, firstCharacter, lastCharacter] = Problem9(UserInput)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+lengthOfString = strlength(UserInput);
+firstCharacter = word(1);
+lastCharacter = word(lengthofString);
+
+end
