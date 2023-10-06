b7d90683c27f457a25fc63ed5e22afe329427110
hancockam21 <hancockam21@students.ecu.edu>
2023-10-05 20:29:50 -0400

    Final

--- assignment3/Problem8.m
+++ assignment3/Problem8.m
@@ -6,7 +6,6 @@ function Problem8(number1,number2)
 % code is also written to automatically count from the smaller number to
 % the larger number no matter what order they are inputted whenc alling the
 % function. 
-
 if number1 > number2
     disp(number2:1:number1)
 elseif number2 > number1 
