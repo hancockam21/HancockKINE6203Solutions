36b4b8aea9014e2b65f7c6223263bfb83b3c725b
hancockam21 <143009128+hancockam21@users.noreply.github.com>
2023-11-15 20:08:42 -0500

    Delete subtracts.m

--- subtracts.m
+++
@@ -1,9 +0,0 @@
-function subtracts(inputArg1,inputArg2)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-if (inputArg1 > inputArg2)
-    inputArg1 - inputArg2
-else 
-    inputArg2 - inputArg1
-end
-end
