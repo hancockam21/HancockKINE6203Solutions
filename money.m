e0af789f8104d9aa2eca2bdc100005323e7c4c4f
hancockam21 <143009128+hancockam21@users.noreply.github.com>
2023-11-15 20:07:34 -0500

    Delete money.m

--- money.m
+++
@@ -1,5 +0,0 @@
-function [totalCoins] = money(quarters, dimes, nickels, pennies)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-totalCoins = (0.25*quarters) + (0.10*dimes) + (0.05*nickels) + (0.01*pennies) ;
-end
